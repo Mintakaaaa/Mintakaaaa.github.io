73c7da2bd3090b92cfc32af837d5a60a96159816
Mintakaaaa <mindaugasb04@gmail.com>
2024-08-12 14:32:58 +0100

    fixed contact footer for case study oops

--- projects/case_study/js/minima.js
+++ projects/case_study/js/minima.js
@@ -131,6 +131,34 @@ document.addEventListener("DOMContentLoaded", function(e) {
   });
   //----
 
+  //----------------------------
+
+  let copiedText = document.querySelector("#copied-text");
+  let copyButton = document.querySelector("#copy-email");
+  copyButton.addEventListener("click", function() {
+    navigator.clipboard.writeText("mindaugasb04@gmail.com");
+    showCopiedAndHide(copiedText);
+  });
+
+  function showCopiedAndHide(copiedText) {
+    gsap.killTweensOf(copiedText); // stops animation glitching when repeatedly clicking super fast
+
+    let tl = gsap.timeline();
+    tl.to(copiedText, {
+      duration: 0.1,
+      opacity: 1,
+      ease: "power1.inOut"
+    })
+    .to(copiedText, {
+      duration: 0.3,
+      opacity: 0,
+      delay: 1,
+      ease: "power1.inOut"
+    });
+  }
+
+  //----
+
   // for when clicking on case study from projects, to slide to specific project type.. like case study on App clicked, go to app case study. duh.
   let hash = window.location.hash; // i.e. minima.html#app  
   if (hash) {
